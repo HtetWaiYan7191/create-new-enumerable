8cc95266f62a4f050f004edeac9d91fe8a9b220e
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-08-22 11:55:33 +0630

    Set up project

---
+++ my_list.rb
@@ -0,0 +1,27 @@
+require_relative 'enumerable'
+
+class MyList
+  include MyEnumerable
+  def initialize(*list)
+    @list = list
+  end
+
+  def show_list
+    print @list
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
+
+list = MyList.new(1, 2, 3, 4)
+list.show_list
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
+
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+
+# Test #filter
+print(list.filter?(&:even?))
